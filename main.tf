8c74b34f23c52d87bf52430fc96fef2422343c91
Ram Vennam <rvennam987@gmail.com>
2017-08-08 10:35:48 -0500

    initial drop

--- main.tf
+++ main.tf
@@ -2,20 +2,6 @@ provider "ibm" {
   bluemix_api_key    = "${var.bx_api_key}"
 }
 
-resource "ibm_container_cluster" "testacc_cluster" {
-  name         = "test"
-  datacenter   = "dal10"
-  org_guid     = "${var.bx_org_guid}"
-  space_guid   = "${var.bx_space_guid}"
-  account_guid = "${var.bx_account_guid}"
-  machine_type = "free"
-
- workers = [{
-    name   = "worker1"
-    action = "add"
-  }]
-}
-
 variable "bx_api_key" {
   type        = "string"
   description = "Your Bluemix API key."
@@ -26,7 +12,7 @@ variable "bx_org_guid" {
   description = "Your Bluemix org GUID."
 }
 
-variable "data.ibm_space.space.id" {
+variable "bx_space_guid" {
   type        = "string"
   description = "Your Bluemix space GUID."
 }
@@ -43,7 +29,7 @@ data "ibm_space" "space" {
 
 resource "ibm_app" "app" {
   name         = "rvennam-tf-app"
-  space_guid   = "${data.ibm_space.space.id}"
+  space_guid   = "${var.bx_space_guid}"
   app_path     = "hello.zip"
   wait_timeout = 90
   buildpack    = "sdk-for-nodejs"
