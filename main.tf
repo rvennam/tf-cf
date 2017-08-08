01371585c8363a310fe6dd34fe4d56d6bc7ceb2b
Ram Vennam <rvennam987@gmail.com>
2017-08-08 11:14:23 -0500

    route

--- main.tf
+++ main.tf
@@ -22,6 +22,12 @@ variable "bx_account_guid" {
   description = "Your Bluemix account GUID."
 }
 
+data "ibm_app_route" "route" {
+  domain_guid = "${data.ibm_app_domain_shared.domain.id}"
+  space_guid  = "${data.ibm_space.spacedata.id}"
+  host        = "rvennam-tf-cf"
+}
+
 data "ibm_space" "space" {
   org   = "rvennam@us.ibm.com"
   space = "dev"
@@ -32,6 +38,7 @@ resource "ibm_app" "app" {
   space_guid   = "${var.bx_space_guid}"
   app_path     = "hello.zip"
   buildpack    = "sdk-for-nodejs"
+  route_guid  = "${data.ibm_app_route.route.id}"
 }
 
 data "ibm_app_domain_shared" "domain" {
@@ -41,10 +48,4 @@ data "ibm_app_domain_shared" "domain" {
 data "ibm_space" "spacedata" {
   space = "dev"
   org   = "rvennam@us.ibm.com"
-}
-
-resource "ibm_app_route" "route" {
-  domain_guid = "${data.ibm_app_domain_shared.domain.id}"
-  space_guid  = "${data.ibm_space.spacedata.id}"
-  host        = "rvennam-tf-cf"
 }
